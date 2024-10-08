992929df1b3d6948de85b3b1cc0b6fed1950a4e5
Richard Streu <rstreu13@icloud.com>
2024-10-08 19:12:09 +0200

    fiexd problem with commenting 'empty import'

--- js/apiService.js
+++ js/apiService.js
@@ -1,59 +1,59 @@
-let users = [];
+// let users = [];
 
-const baseUrl ="https://join-storage-460c8-default-rtdb.europe-west1.firebasedatabase.app/";
+// const baseUrl ="https://join-storage-460c8-default-rtdb.europe-west1.firebasedatabase.app/";
 
-async function postData(path = "", data = {}) {
-  let response = await fetch(baseUrl + path + ".json", {
-    method: "POST",
-    header: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  return (responseAsJson = await response.json());
-}
+// async function postData(path = "", data = {}) {
+//   let response = await fetch(baseUrl + path + ".json", {
+//     method: "POST",
+//     header: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   });
+//   return (responseAsJson = await response.json());
+// }
 
-async function addContact() {
-  let name = document.getElementById("name").value;
-  let surname = document.getElementById("surname").value;
-  
-  let userInitials = name.charAt(0) + surname.charAt(0);
+// async function addContact() {
+//   let name = document.getElementById("name").value;
+//   let surname = document.getElementById("surname").value;
 
-  let id = userInitials + Date.now();
-  let user = {
-    Name: name,
-    Surname: surname,
-    Id: id,
-    UserInitials: userInitials,
-  };
+//   let userInitials = name.charAt(0) + surname.charAt(0);
 
-  let response = await fetch(baseUrl + "/user/" + id + ".json", {
-    method: "PATCH",
-    header: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  });
-  document.getElementById("name").value = "";
-  document.getElementById("surname").value = "";
-  return (responseAsJson = await response.json());
-}
+//   let id = userInitials + Date.now();
+//   let user = {
+//     Name: name,
+//     Surname: surname,
+//     Id: id,
+//     UserInitials: userInitials,
+//   };
 
-async function loadContent() {
-  let response = await fetch(baseUrl + ".json");
-  let responseAsJson = await response.json();
-  console.log(responseAsJson);
-  users = Object.entries(responseAsJson.user);
-  console.log(users);
+//   let response = await fetch(baseUrl + "/user/" + id + ".json", {
+//     method: "PATCH",
+//     header: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(user),
+//   });
+//   document.getElementById("name").value = "";
+//   document.getElementById("surname").value = "";
+//   return (responseAsJson = await response.json());
+// }
 
-  for (let index = 0; index < users.length; index++) {
-    document.getElementById("content").innerHTML += /*html*/ `
-    <div id="user">
-      <p>${users[index][1].Name}</p>
-      <p>${users[index][1].Surname}</p>
-      <p id="initials">${users[index][1].UserInitials}</p>
-    </div>
-      
-    `;
-  }
-}
+// async function loadContent() {
+//   let response = await fetch(baseUrl + ".json");
+//   let responseAsJson = await response.json();
+//   console.log(responseAsJson);
+//   users = Object.entries(responseAsJson.user);
+//   console.log(users);
+
+//   for (let index = 0; index < users.length; index++) {
+//     document.getElementById("content").innerHTML += /*html*/ `
+//     <div id="user">
+//       <p>${users[index][1].Name}</p>
+//       <p>${users[index][1].Surname}</p>
+//       <p id="initials">${users[index][1].UserInitials}</p>
+//     </div>
+
+//     `;
+//   }
+// }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-import { renderContactList } from "../components/contactList/contactList.js";
-renderContactList();
+// import { renderContactList } from "../components/contactList/contactList.js";
+// renderContactList();
 
-import { showAddNewUserDialog, hideAddNewUserDialog } from "../components/contactModal.js";
+import { showAddNewUserDialog, hideAddNewUserDialog } from "../components/contactModal/contactModal.js";
 window.showAddNewUserDialog = showAddNewUserDialog;
 window.hideAddNewUserDialog = hideAddNewUserDialog;
