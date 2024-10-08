11b6cb7d8b3c4a940da35fc6aa110366eea11dba
Richard Streu <rstreu13@icloud.com>
2024-10-08 18:21:42 +0200

    wrote functions to show / hide add new user dialog, importet them to script.js

---
+++ components/contactModal/contactModal.js
@@ -0,0 +1,7 @@
+export function showAddNewUserDialog() {
+  document.getElementById("contactModal").classList.remove("d_none");
+}
+
+export function hideAddNewUserDialog() {
+  document.getElementById("contactModal").classList.add("d_none");
+}
--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 import { renderContactList } from "../components/contactList/contactList.js";
 renderContactList();
+
+import { showAddNewUserDialog, hideAddNewUserDialog } from "../components/contactModal.js";
+window.showAddNewUserDialog = showAddNewUserDialog;
+window.hideAddNewUserDialog = hideAddNewUserDialog;
