269ff950448e6fe4f85ebbdad09bcf95975601b7
Richard Streu <rstreu13@icloud.com>
2024-10-08 20:43:53 +0200

    added userColors arrac to contactModal.js

--- components/contactModal/contactModal.js
+++ components/contactModal/contactModal.js
@@ -1,5 +1,23 @@
 import { addContactNameInputRef, addContactEmailInputRef, addContactPhoneInputRef } from "../../js/script.js";
 
+let userColors = [
+  "#FF7A00",
+  "#FF5EB3",
+  "#6E52FF",
+  "#9327FF",
+  "#00BEE8",
+  "#1FD7C1",
+  "#FF745E",
+  "#FFA35E",
+  "#FC71FF",
+  "#FFC701",
+  "#0038FF",
+  "#C3FF2B",
+  "#FFE62B",
+  "#FF4646",
+  "#FFBB2B",
+];
+
 export function getNewUser() {
   let fullName = addContactNameInputRef.value;
   let nameParts = fullName.split(" ");
