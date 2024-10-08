67cf30371e56115c1eb56fbdaf4d5f7d9ade7ddf
Richard Streu <rstreu13@icloud.com>
2024-10-08 22:03:18 +0200

    function loadUsers added and ready

--- js/apiService.js
+++ js/apiService.js
@@ -1,38 +1,29 @@
-let users = [];
-
 const baseUrl = "https://join-storage-460c8-default-rtdb.europe-west1.firebasedatabase.app/";
 
 import { getNewUser } from "../components/contactModal/contactModal.js";
 
-export async function addContact() {
-  let user = getNewUser();
-  let id = user.id;
+// export let usersArray = [];
 
+export async function addContact() {
+  await patchNewUser();
+  await loadUsers();
+}
+
+async function patchNewUser() {
+  let newUser = getNewUser();
+  let id = newUser.id;
   let response = await fetch(baseUrl + "/user/" + id + ".json", {
     method: "PATCH",
     header: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(user),
+    body: JSON.stringify(newUser),
   });
-  console.log(user, id);
 }
 
-async function loadContent() {
+export async function loadUsers() {
   let response = await fetch(baseUrl + ".json");
   let responseAsJson = await response.json();
-  console.log(responseAsJson);
-  users = Object.entries(responseAsJson.user);
-  console.log(users);
-
-  for (let index = 0; index < users.length; index++) {
-    document.getElementById("content").innerHTML += /*html*/ `
-    <div id="user">
-      <p>${users[index][1].Name}</p>
-      <p>${users[index][1].Surname}</p>
-      <p id="initials">${users[index][1].UserInitials}</p>
-    </div>
-      
-    `;
-  }
+  let users = Object.entries(responseAsJson.user);
+  return users;
 }
--- js/script.js
+++ js/script.js
@@ -5,9 +5,16 @@ import { showAddNewUserDialog, hideAddNewUserDialog } from "../components/contac
 window.showAddNewUserDialog = showAddNewUserDialog;
 window.hideAddNewUserDialog = hideAddNewUserDialog;
 
-import { addContact } from "../js/apiService.js";
+import { addContact, loadUsers } from "../js/apiService.js";
 window.addContact = addContact;
 
 export let addContactNameInputRef = document.getElementById("addContactNameInput");
 export let addContactEmailInputRef = document.getElementById("addContactEmailInput");
 export let addContactPhoneInputRef = document.getElementById("addContactPhoneInput");
+
+async function getUsersArray() {
+  let usersArray = await loadUsers();
+  console.log(usersArray);
+}
+
+getUsersArray();
