6f5e9043ccf29ccc96cd2f97fb764e314d5c6110
DavidDev25 <david.werner@david-werner.dev>
2024-10-09 18:03:30 +0200

    added active states, added container in empty files for test purposes

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -1,17 +1,20 @@
-// const sideBarElements = document.querySelectorAll('sidebar-Block a');
-// const windowPathName = window.location.pathname;
-
-// sideBarElements.forEach(sideBarElements => {
-//     const sideBarElementsPathname = new URL(sideBarElement.href).pathname;
-//     if (windowPathName == sideBarElementsPathname) {
-//         sideBarElements.classList.add('active');
-//     }
-// });
-
-
 export function renderSidebar() {
     const sidebarRef = document.getElementById('sidebar');
     sidebarRef.innerHTML += renderSidebarTemplate()
+    const pageIdMap = {
+        '/contacts.html': 'contacts',
+        '/summary.html': 'summary',
+        '/addTask.html': 'addTask',
+        '/board.html': 'board',
+        '/privacyPolicy.html': 'privacyPolicy',
+        '/legalNotice.html': 'legalNotice',
+    }
+    const currentPath = window.location.pathname;
+    const activeElementId = pageIdMap[currentPath];
+    if (activeElementId) {
+        const activeElement = document.getElementById(activeElementId);
+        activeElement.classList.add('active');
+    }
 }
 
 function renderSidebarTemplate() {
@@ -20,14 +23,14 @@ function renderSidebarTemplate() {
         <img src="/assets/icons/joinLogo.png" />
     </div>
         <div class="sidebar-Block">
-            <a id="summary" href="/summary.html"><img src="/assets/icons/Summary.png" /></a>
-            <a id="addTask" href="/addTask.html"><img src="/assets/icons/addTask.png" /></a>
-            <a id="board" href="/board.html"><img src="/assets/icons/board.png" /></a>
-            <a id="contacts" href="/contacts.html"><img src="/assets/icons/contact.png" /></a>
+            <a id="summary" class="" href="/summary.html"><img src="/assets/icons/Summary.png" /></a>
+            <a id="addTask" class="" href="/addTask.html"><img src="/assets/icons/addTask.png" /></a>
+            <a id="board" class="" href="/board.html"><img src="/assets/icons/board.png" /></a>
+            <a id="contacts" class="" href="/contacts.html"><img src="/assets/icons/contact.png" /></a>
         </div>
         <div id="informations">
-            <a href="/privacyPolicy.html"><img src="/assets/icons/privacyPolicy.png" /></a>
-            <a href=" /legalNotice.html"><img src="/assets/icons/legalNotice.png" /></a>
+            <a id="privacyPolicy" href="/privacyPolicy.html"><img src="/assets/icons/privacyPolicy.png" /></a>
+            <a id="legalNotice" href="/legalNotice.html"><img src="/assets/icons/legalNotice.png" /></a>
     </div>    
 `
 }
