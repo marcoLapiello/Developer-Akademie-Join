31848d4ea0b3a4088e7cb05ddcb9b15d5c6bf468
DavidDev25 <david.werner@david-werner.dev>
2024-10-09 02:45:05 +0200

    removed some tipos

---
+++ components/sidebar/sidebar.js
@@ -0,0 +1,11 @@
+//const sideBarElements = document.querySelectorAll('sidebar-Block a');
+// const windowPathName = window.location.pathname;
+
+// sideBarElements.forEach(sideBarElements => {
+//     const sideBarElementsPathname = new URL(sideBarElement.href).pathname;
+
+
+//     if (windowPathName == sideBarElementsPathname) {
+//         sideBarElements.classList.add('active');
+//     }
+// });
