6cd5101094e7cd7077a5b152163296dd450c07c5
Mirko Rinke <mirkorinke@htp.com>
2024-10-10 20:46:25 +0200

    Refactor sidebar rendering logic and update active element highlighting

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -11,12 +11,12 @@ function renderSidebarTemplate() {
           <img src="/assets/icons/joinLogo.png" />
       </div>
           <div class="sidebar-Block">
-              <a  class="${urlPath === "/summary.html" ? "active" : ""}" href="/summary.html"><img src="/assets/icons/summaryicon.png" />Summary</a>
+              <a  class="${urlPath === "/summary.html" ? "active" : ""}" href="/summary.html"><img src="/assets/icons/summaryIcon.png" />Summary</a>
               <a  class="${urlPath === "/addTask.html" ? "active" : ""}" href="/addTask.html"><img src="/assets/icons/addTaskIcon.png" />Add Task</a>
-              <a  class="${urlPath === "/board.html" ? "active" : ""}" href="/board.html"><img src="/assets/icons/boardicon.png" />Board</a>
+              <a  class="${urlPath === "/board.html" ? "active" : ""}" href="/board.html"><img src="/assets/icons/boardIcon.png" />Board</a>
               <a  class="${urlPath === "/contacts.html" ? "active" : ""}" href="/contacts.html"><img src="/assets/icons/contactIcon.png" />Contacts</a>
           </div>
-          <div id="informations">
+          <div id="information">
               <a class="${urlPath === "/privacyPolicy.html" ? "active" : ""}" id="privacyPolicy" href="/privacyPolicy.html">Privacy Policy</a>
               <a class="${urlPath === "/legalNotice.html" ? "active" : ""}" id="legalNotice" href="/legalNotice.html">Legal Notice</a>
       </div>
