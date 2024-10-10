328349888f50e7de76fbb063ad91fdbc6ac76ec3
DavidDev25 <david.werner@david-werner.dev>
2024-10-10 20:20:23 +0200

    fixed sidebar issues, replaced Images

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -25,8 +25,8 @@ function renderSidebarTemplate() {
         <div class="sidebar-Block">
             <a  class="" href="/summary.html"><img src="/assets/icons/summaryicon.png" />Summary</a>
             <a  class="" href="/addTask.html"><img src="/assets/icons/addTaskIcon.png" />Add Task</a>
-            <a  class="" href="/board.html"><img src="/assets/icons/boardicon.png" />Board</a>
-            <a  class="" href="/contacts.html"><img src="/assets/icons/contactIcon.png" />Contacts</a>
+            <a class ="" href="/board.html"><img src="/assets/icons/boardicon.png" /> Block </a>
+            <a  class="" href="/contacts.html"><img src="/assets/icons/contactIcon.png" /> Contacts</a>
         </div>
         <div id="informations">
             <a id="privacyPolicy" href="/privacyPolicy.html">Privacy Policy</a>
