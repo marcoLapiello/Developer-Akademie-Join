4974daf054b618ed15ceaff1dd1f8015c808055f
DavidDev25 <david.werner@david-werner.dev>
2024-10-11 11:22:22 +0200

    fixed typo in questionmark_small in to questionMark_small.png

--- components/header/header.js
+++ components/header/header.js
@@ -1,15 +1,15 @@
 export function toggle_d_None() {
-    let userMenu = document.getElementById("dropDown");
-    userMenu.classList.toggle("d_None");
+  let userMenu = document.getElementById("dropDown");
+  userMenu.classList.toggle("d_None");
 }
 
 function renderHeaderTemplate() {
-    return /*html*/`
+  return /*html*/`
         <header>
       Kanban Project Management Tool
       <div class="header-rightSide">
         <a href="/help.html">
-          <img src="/assets/icons/questionmark_small.png" alt="Help" />
+          <img src="/assets/icons/questionMark_small.png" alt="Help" />
         </a>
         <div onclick="toggle_d_None()" id="user_Profile_Initials" class="user-Profile-Initials">
           <div class="dropDown">
