915bd0cdd038fdd3808a50694c75383aceb80ccc
DavidDev25 <david.werner@david-werner.dev>
2024-10-14 12:31:43 +0200

    added div container to sidebar.js for responsive design.

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -10,20 +10,26 @@ function renderSidebarTemplate() {
     
       <div class="sidebarUpperPart">
         <img class= "joinLogo"  src="/assets/icons/joinLogo.png" />
-        <div class="sidebar-Block">
+      <div class="sidebar-Block">
 
-        <!-- <div class="${urlPath === "/summary.html" ? "active" : ""} navElement">
+        <div class="${urlPath === "/summary.html" ? "active" : ""} navElement">
           <img src="/assets/icons/summaryIcon.png" alt="">  
           <a href="">Summary</a>
-        </div> -->
-
-
-          <a  class="${urlPath === "/summary.html" ? "active" : ""}" href="/summary.html"><img src="/assets/icons/summaryIcon.png" />Summary</a>
-          <a  class="${urlPath === "/addTask.html" ? "active" : ""}" href="/addTask.html"><img src="/assets/icons/addTaskIcon.png" />Add Task</a>
-          <a  class="${urlPath === "/board.html" ? "active" : ""}" href="/board.html"><img src="/assets/icons/boardIcon.png" />Board</a>
-          <a  class="${urlPath === "/contacts.html" ? "active" : ""}" href="/contacts.html"><img src="/assets/icons/contactIcon.png" />Contacts</a>
+        </div> 
+        <div class="${urlPath === "/addTask.html" ? "active" : ""} navElement">
+          <img src="/assets/icons/addTaskIcon.png" alt="">  
+          <a href="">Add Task</a>
+        </div>
+        <div class="${urlPath === "/board.html" ? "active" : ""} navElement">
+          <img src="/assets/icons/boardIcon.png" alt="">  
+          <a href="">Board</a>
+        </div>
+        <div class="${urlPath === "/contacts.html" ? "active" : ""} navElement">
+          <img src="/assets/icons/contactIcon.png" alt="">  
+          <a href="">Contacts</a>
         </div>
       </div>
+
       <div id="information">
         <a class="${urlPath === "/privacyPolicy.html" ? "active" : ""}" id="privacyPolicy" href="/privacyPolicy.html">Privacy Policy</a>
         <a class="${urlPath === "/legalNotice.html" ? "active" : ""}" id="legalNotice" href="/legalNotice.html">Legal Notice</a>
