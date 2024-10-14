9909b8ba463059d9c313ea5ef83768e2dc3b5a13
DavidDev25 <david.werner@david-werner.dev>
2024-10-14 19:40:38 +0200

    fixed filepath with missing dots

--- components/header/header.js
+++ components/header/header.js
@@ -16,20 +16,20 @@ function renderHeaderTemplate() {
       <span>Kanban Project Management Tool</span>
       </div>
       <div class="header-rightSide">
-        <a href="/help.html">
-          <img src="/assets/icons/questionmark_small.png" alt="Help" />
+        <a href="./help.html">
+          <img src="./assets/icons/questionmark_small.png" alt="Help" />
         </a>
         <div onclick="toggle_d_None()" id="user_Profile_Initials" class="user-Profile-Initials">
           <span>G</span>
           <div class="dropDown d_none" id="dropDown">
           <div class="legalNotice">
-            <a href="/legalNotice.html">Legal Notice</a>
+            <a href="./legalNotice.html">Legal Notice</a>
           </div>
           <div class="privacyPolicy">
-            <a href="/privacyPolicy.html">Privacy Policy</a>
+            <a href="./privacyPolicy.html">Privacy Policy</a>
           </div>
           <div class="logOut">
-            <a href="/login.html">Log out</a>
+            <a href="./login.html">Log out</a>
           </div>
         </div>
       </div>
--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -9,18 +9,18 @@ function renderSidebarTemplate() {
   return /*html*/ `
     
       <div class="sidebarUpperPart">
-        <img class= "joinLogo"  src="/assets/icons/joinLogo.png" />
+        <img class= "joinLogo"  src="./assets/icons/joinLogo.png" />
         <div class="sidebar-Block">
-          <a class="${urlPath === "/summary.html" ? "active" : ""}" href="/summary.html"><img src="/assets/icons/summaryicon.png" />Summary</a>
-          <a class="${urlPath === "/addTask.html" ? "active" : ""}" href="/addTask.html"><img src="/assets/icons/addTaskIcon.png" />Add Task</a>
-          <a class="${urlPath === "/board.html" ? "active" : ""}" href="/board.html"><img src="/assets/icons/boardicon.png" />Board</a>
-          <a class="${urlPath === "/contacts.html" ? "active" : ""}" href="/contacts.html"><img src="/assets/icons/contactIcon.png" />Contacts</a>
+          <a class="${urlPath === "./summary.html" ? "active" : ""}" href="/summary.html"><img src="./assets/icons/summaryicon.png" />Summary</a>
+          <a class="${urlPath === "./addTask.html" ? "active" : ""}" href="/addTask.html"><img src="./assets/icons/addTaskIcon.png" />Add Task</a>
+          <a class="${urlPath === "./board.html" ? "active" : ""}" href="/board.html"><img src="./assets/icons/boardicon.png" />Board</a>
+          <a class="${urlPath === "./contacts.html" ? "active" : ""}" href="/contacts.html"><img src="./assets/icons/contactIcon.png" />Contacts</a>
         </div>
       </div>
 
       <div id="information">
-        <a class="${urlPath === "/privacyPolicy.html" ? "active" : ""}" id="privacyPolicy" href="/privacyPolicy.html">Privacy Policy</a>
-        <a class="${urlPath === "/legalNotice.html" ? "active" : ""}" id="legalNotice" href="/legalNotice.html">Legal Notice</a>
+        <a class="${urlPath === "./privacyPolicy.html" ? "active" : ""}" id="privacyPolicy" href="./privacyPolicy.html">Privacy Policy</a>
+        <a class="${urlPath === "./legalNotice.html" ? "active" : ""}" id="legalNotice" href="./legalNotice.html">Legal Notice</a>
       </div>
   `;
 }
