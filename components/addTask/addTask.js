6734c2b8069e13a9f81226e113fe7e3889ae6ca8
Mirko Rinke <mirkorinke@htp.com>
2024-10-17 00:12:28 +0200

    Refactor addTask.js to update data structure and property names

--- components/addTask/addTask.js
+++ components/addTask/addTask.js
@@ -1,35 +1,31 @@
-// cards = {
-//     "TASK3757435747": {
-//       "id": "TASK123456",
-//       "title": "Implement Search Functionality",
-//       "description": "Develop the search functionality to filter tasks on the board.",
-//       "assignedTo": [
-//         "JD3757435747",  // User ID von John Doe
-//         "AS4577547547",  // User ID von Alice Smith
-//         "BJ8657363623"   // User ID von Bob Johnson
-//       ],
-//       "dueDate": "2024-10-30",
-//       "creationDate: "8657363623",
-//       "creatorId: AS4577547547",
-//       "priority": "Urgent",
-//       "category": "Technical Task",
-//       "progress": 50,  // Fortschrittsbalken für Subtasks
-//       "status": "In Progress",  // Spalte, in der sich die Task befindet
-//       "subtasks": [
-//         {
-//           "id": "SUBTASK3757435747",
-//           "task": "Create search input",
-//           "isDone": "true"
-//           "creationDate: "8657363676",
-//           "creatorId: AS4577547547",
-//         },
-//         {
-//           "id": "SUBTASK3794346851",
-//           "task": "Connect search to database",
-//           "isDone": "false"
-//           "creationDate: "8657363644",
-//           "creatorId: AS4577547547",
-//         }
-//       ]
-//     }
-//   };
+const tempCards = {
+  TASK3757435747: {
+    id: "TASK123456",
+    title: "Implement Search Functionality",
+    description: "Develop the search functionality to filter tasks on the board.",
+    assignedTo: ["CK1728938463133", "MH1729079944158", "MS1728938311249"],
+    dueDate: "2024-10-30",
+    creationDate: "8657363623",
+    creatorId: "AS4577547547",
+    priority: "Urgent",
+    category: "Technical Task",
+    progress: 50,
+    status: "In Progress",
+    subtasks: [
+      {
+        id: "SUBTASK3757435747",
+        task: "Create search input",
+        isDone: true,
+        creationDate: "8657363676",
+        creatorId: "AS4577547547",
+      },
+      {
+        id: "SUBTASK3794346851",
+        task: "Connect search to database",
+        isDone: false,
+        creationDate: "8657363644",
+        creatorId: "AS4577547547",
+      },
+    ],
+  },
+};
