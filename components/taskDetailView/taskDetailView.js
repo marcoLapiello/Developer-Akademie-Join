2b5436f450d25786dbeb0ea6b57364f0bb495e1b
Mirko Rinke <mirkorinke@htp.com>
2024-10-17 20:42:11 +0200

    Refactor taskDetailView.js to fetch and render task data from the database

--- components/taskDetailView/taskDetailView.js
+++ components/taskDetailView/taskDetailView.js
@@ -1,63 +1,29 @@
-async function returnTempCards() {
-  const tempCards = {
-    TASK3757435747: {
-      id: "TASK3757435747",
-      title: "Implement Search Functionality",
-      description: "Develop the search functionality to filter tasks on the board.",
-      assignedTo: { CK1728938463133: "CK1728938463133", MH1729079944158: "MH1729079944158", MS1728938311249: "MS1728938311249" },
-      dueDate: "2024-10-30",
-      creationDate: "8657363623",
-      creatorId: "AS4577547547",
-      priority: "Urgent",
-      category: "Technical Task",
-      categoryColor: "#0038ff",
-      progress: 50,
-      status: "In Progress",
-      subtasks: {
-        SUBTASK3757435747: {
-          id: "SUBTASK3757435747",
-          task: "Create search input",
-          isDone: true,
-          creationDate: "8657363676",
-          creatorId: "AS4577547547",
-        },
-        SUBTASK3794346851: {
-          id: "SUBTASK3794346851",
-          task: "Connect search to database",
-          isDone: false,
-          creationDate: "8657363644",
-          creatorId: "AS4577547547",
-        },
-      },
-    },
-  };
-  return tempCards;
-}
-
 import { returnIcon } from "../icons.js";
-import { getUsersArray } from "../../js/script.js";
+import { getUsersArray, getTasksArray } from "../../js/script.js";
 
 // The function below is a placeholder until we have the real data structure
 // and can fetch the data from the database to render the task detail view.
 export async function renderTaskDetailView() {
-  let taskID = "TASK3757435747";
-  let tasksArray = await returnTempCards(); // Placeholder until we have the real data structure
-  let assignedUsers = await renderAssignedUser(taskID, tasksArray);
-  let subtasks = await renderSubtasks(taskID, tasksArray);
-  const taskDetailViewRef = document.getElementById("taskDetailView");
-  if (taskDetailViewRef) taskDetailViewRef.innerHTML = renderTaskDetailViewTemplate(tasksArray[taskID], assignedUsers, subtasks);
+  let taskID = "TASK3757435747"; // Temporary task ID
+  let tasksArray = await getTasksArray(); // Fetch tasks array
+  let assignedUsers = await renderAssignedUser(taskID, tasksArray); // Fetch assigned users for the task and render them
+  let subtasks = await renderSubtasks(taskID, tasksArray); // Fetch subtasks for the task and render them
+  const taskDetailViewRef = document.getElementById("taskDetailView"); // Get task detail view element
+  const taskData = tasksArray.find(([id]) => id === taskID)[1]; // Find the task data for the task ID in the tasks array
+  if (taskDetailViewRef && taskData) taskDetailViewRef.innerHTML = renderTaskDetailViewTemplate(taskData, assignedUsers, subtasks); // Render the task detail view template with the task data, assigned users and subtasks
 }
 
 // The function find the assigned users for a task and returns the user data
 // for each user assigned to the task.
 export async function getAssignedUsersData(taskID, tasksArray) {
-  let usersArray = await getUsersArray();
-  const assignedTo = Object.values(tasksArray[taskID].assignedTo); // Change: Extract values from the assignedTo object
+  let usersArray = await getUsersArray(); // Fetch users array
+  const taskData = tasksArray.find(([id]) => id === taskID)[1]; // Find the task data for the task ID in the tasks array
+  const assignedTo = Object.values(taskData.assignedTo); // Get the assigned users for the task
   const assignedUsers = [];
-  const users = Object.values(usersArray).flat();
   assignedTo.forEach((userId) => {
-    const user = users.find((user) => user.id === userId);
-    if (user) assignedUsers.push(user);
+    // Find the user data for each user assigned to the task and push it to the assignedUsers array
+    const user = usersArray.find((usersGroup) => usersGroup[1].id === userId);
+    if (user) assignedUsers.push(user[1]);
   });
   return assignedUsers;
 }
@@ -70,12 +36,17 @@ export function toggleTaskDetailView() {
 
 // The function renders the assigned users for a task and returns the user data as HTML
 async function renderAssignedUser(taskID, tasksArray) {
-  let assignedUsers = await getAssignedUsersData(taskID, tasksArray);
+  let assignedUsers = await getAssignedUsersData(taskID, tasksArray); // Fetch assigned users for the task and render them
   let userListHTML = "";
   assignedUsers.forEach((user) => {
     if (user) {
-      userListHTML += /*html*/ `    
-        <li class="userName"><span class="userInitials" style="background-color: ${user.user_color}">${user.profile.initials}</span>${user.profile.first_name} ${user.profile.last_name}</li>
+      userListHTML += /*html*/ `
+        <li class="userName">
+          <span class="userInitials" style="background-color: ${user.user_color}">
+            ${user.profile.initials}
+          </span>
+          ${user.profile.first_name} ${user.profile.last_name}
+        </li>
       `;
     }
   });
@@ -84,32 +55,39 @@ async function renderAssignedUser(taskID, tasksArray) {
 
 // The function renders the subtasks for a task and returns the subtasks as HTML
 async function renderSubtasks(taskID, tasksArray) {
-  let subtasks = Object.values(tasksArray[taskID].subtasks); // Change: Extract values from the subtasks object
+  const taskData = tasksArray.find(([id]) => id === taskID)[1]; // Find the task data for the task ID in the tasks array
+  const subtasks = Object.values(taskData.subtasks); // Get the subtasks for the task
   let subtasksListHTML = "";
   subtasks.forEach((subtask) => {
+    // Render the subtasks as HTML and push them to the subtasksListHTML
     if (subtask) {
       subtasksListHTML += /*html*/ `
-      <li class="subtask" id="subtasksID${subtask.id}">
-        <input onchange="checkedSubtask(event,'${taskID}')" type="checkbox" name="${subtask.id}" id="${subtask.id}" class="checkboxSubtask" ${subtask.isDone ? "checked" : ""}>
-        ${subtask.task}
-      </li>
+        <li class="subtask" id="subtasksID${subtask.id}">
+          <input 
+            onchange="checkedSubtask(event,'${taskID}')" 
+            type="checkbox" 
+            name="${subtask.id}" 
+            id="${subtask.id}" 
+            class="checkboxSubtask" 
+            ${subtask.isDone ? "checked" : ""}>
+          ${subtask.task}
+        </li>
       `;
     }
   });
   return subtasksListHTML;
 }
 
+// The function checks a subtask and updates the task data with the new subtask status
+// and returns the updated task data
 export async function checkedSubtask(event, taskID) {
-  let tasksArray = await returnTempCards(); // Placeholder until real data structure is available
-  let currentTask = tasksArray[taskID]; // Get the current task by taskID
-  let checkboxId = event.target.id;
-  let isChecked = event.target.checked;
-  const foundSubtask = currentTask.subtasks[checkboxId]; // Change: Access subtask by its key in the object
-  if (foundSubtask) foundSubtask.isDone = isChecked;
-  tasksArray[taskID] = currentTask;
-  console.log(tasksArray[taskID]);
-
-  return tasksArray[taskID];
+  let tasksArray = await getTasksArray(); // Fetch tasks array to get the task data for the task ID
+  const taskData = tasksArray.find(([id]) => id === taskID)[1]; // Find the task data for the task ID in the tasks array
+  let checkboxId = event.target.id; // Get the checkbox ID the checkbox is the subtask ID
+  let isChecked = event.target.checked; // Get the status of the checkbox
+  const foundSubtask = taskData.subtasks[checkboxId]; // Find the subtask in the task data with the subtask ID
+  if (foundSubtask) foundSubtask.isDone = isChecked; // Update the subtask status with the new status
+  // pushToDatabase(taskData); // The is a example function to push the updated task data to the database. The Funktion is not implemented yet.
 }
 
 // The function renders the task detail view template with the subtasks and assigned users data
