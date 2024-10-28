1cb3498bfdb9d838a894d3fc4391e44f48dcb47d
marcoLapiello <marcolapiello@gmail.com>
2024-10-28 13:11:56 +0100

    renderSummary() - almost complete - logged in user and next deadline not there yet

--- components/summary/summary.js
+++ components/summary/summary.js
@@ -20,12 +20,112 @@ async function renderSummary() {
     // How many Done (tasks[index][1].status == "done") 
     // How many in Progress (tasks[index][1].status == "inProgress")
     // How many waits for FeedB (tasks[index][1].status == "awaitFeedback")
-    let 
+    let [toDoAmount, inProgressAmount, awaitFeedbackAmount, doneAmount] = getEveryStatusAmount(tasks);
+    console.log(toDoAmount, inProgressAmount, awaitFeedbackAmount, doneAmount);
     
     
-    
-    
+    document.getElementById("summaryContent").innerHTML = getSummaryTemplate(currentTasksAmount, urgentTasksAmount, toDoAmount, inProgressAmount, awaitFeedbackAmount, doneAmount);
 }
+
+function getSummaryTemplate(currentTasksAmount, urgentTasksAmount, toDoAmount, inProgressAmount, awaitFeedbackAmount, doneAmount) {
+    return /*html*/`
+        <div id="summaryWrapper" class="summaryWrapper">
+
+            <div id="summaryHeadline" class="summaryHeadline">
+            <h1>Join 360</h1>
+            <div class="headlineSeparator"></div>
+            <span>Key Metrics at a Glance</span>
+            </div>
+
+            <div id="summaryMiddleContent" class="summaryMiddleContent">
+            <div id="summaryTasksStatus" class="summaryTasksStatus">
+                <div id="statusFirstLine" class="statusFirstLine">
+
+                <div id="toDoWrapper" class="toDoWrapper">
+                    <div id="" class="iconContainer">
+                    <img src="./assets/icons/pencil_white.png" alt="">
+                    </div>
+                    <div id="" class="amountContainer">
+                    <p id="amountToDo" class="amount">${toDoAmount}</p>
+                    <span>To-do</span>
+                    </div>
+                </div>
+
+                <div id="DoneWrapper" class="toDoWrapper">
+                    <div id="" class="iconContainer">
+                    <img src="./assets/icons/check_white_fat.png" alt="">
+                    </div>
+                    <div id="" class="amountContainer">
+                    <p id="amountDone" class="amount">${doneAmount}</p>
+                    <span>Done</span>
+                    </div>
+                </div>
+                </div>
+
+                <div id="statusSecondLine" class="statusSecondLine">
+                <div id="urgentAmountWrapper" class="urgentAmountWrapper">
+                    <div id="" class="iconContainer">
+                    <img src="./assets/icons/urgent_Icon_white_big.png" alt="">
+                    </div>
+                    <div id="" class="amountContainer">
+                    <p id="amountUrgent" class="amount">${urgentTasksAmount}</p>
+                    <span>Urgent</span>
+                    </div>
+                </div>
+                <div class="urgentSeparator"></div>
+                <div id="urgentDeadlineContainer" class="urgentDeadlineContainer">
+                    <p id="urgentDeadline">complete your function!!</p>
+                    <span>Upcoming Deadline</span>
+                </div>
+                </div>
+
+                <div id="statusThirdLine" class="statusThirdLine">
+                <div id="" class="amountContainer">
+                    <p id="amountUrgent" class="amount">${currentTasksAmount}</p>
+                    <span>Tasks in Board</span>
+                </div>
+                <div id="" class="amountContainer">
+                    <p id="amountUrgent" class="amount">${inProgressAmount}</p>
+                    <span>Tasks In Progress</span>
+                </div>
+                <div id="" class="amountContainer">
+                    <p id="amountUrgent" class="amount">${awaitFeedbackAmount}</p>
+                    <span>Awaiting Feedback</span>
+                </div>
+                </div>
+            </div>
+
+
+            <div id="summaryWelcomeMessage" class="summaryWelcomeMessage">
+                <h2 id="dynamicWelcome">Good morning,</h2>
+                <h1 id="dynamicUser">complete your Function!!</h1>
+            </div>
+            </div>
+        </div>
+    `
+}
+
+
+function getEveryStatusAmount(tasks) {
+    let toDoAmount = 0;
+    let inProgressAmount = 0;
+    let awaitFeedbackAmount = 0;
+    let doneAmount = 0;
+    for (let index = 0; index < tasks.length; index++) {
+        if (tasks[index][1].status == "todo") {
+            toDoAmount++;
+        } else if (tasks[index][1].status == "inProgress") {
+            inProgressAmount++;
+        } else if (tasks[index][1].status == "awaitFeedback") {
+            awaitFeedbackAmount++
+        } else if (tasks[index][1].status == "done") {
+            doneAmount++;
+        }
+        
+    }
+    return [toDoAmount, inProgressAmount, awaitFeedbackAmount, doneAmount]
+}
+
 
 function getUrgentTasksAmount(tasks) {
     let currentTasksAmount = 0;
