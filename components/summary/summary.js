944f29203b57420fc2a7a0045f7c15ab0851a7f0
marcoLapiello <marcolapiello@gmail.com>
2024-10-28 12:45:49 +0100

    renderSummary() - currentTasksAmount and urgentTasksAmount correctly working

--- components/summary/summary.js
+++ components/summary/summary.js
@@ -7,14 +7,34 @@ export function initSummary() {
 async function renderSummary() {
     let tasks = await loadTasks();
     console.log(tasks);
+    // How many in Board (tasks.length)
+    let currentTasksAmount = tasks.length;
+    console.log(currentTasksAmount);
+    // How many Urgent (tasks[index][1].priority == "Urgent")
+    let urgentTasksAmount = getUrgentTasksAmount(tasks);
+    console.log(urgentTasksAmount);
+    // Most urgent deadline (tasks[index][1].dueDate)
+
+
     // How many to do (tasks[index][1].status == "todo")
     // How many Done (tasks[index][1].status == "done") 
-    // How many Urgent (tasks[index][1].priority == "Urgent")
-    // How many in Board (tasks.length)
     // How many in Progress (tasks[index][1].status == "inProgress")
     // How many waits for FeedB (tasks[index][1].status == "awaitFeedback")
+    let 
     
-    // Most urgent deadline (tasks[index][1].dueDate)
+    
+    
+    
+}
+
+function getUrgentTasksAmount(tasks) {
+    let currentTasksAmount = 0;
+    for (let index = 0; index < tasks.length; index++) {
+        if (tasks[index][1].priority == "Urgent") {
+            currentTasksAmount++;
+        }
+    }
+    return currentTasksAmount;
 }
 
 
