8a82fc543575c35a7e94e7a19b2fa1a6df1b9f5e
Mirko Rinke <mirkorinke@htp.com>
2024-10-31 18:47:30 +0100

    Format code: Adjust indentation and line spacing

--- components/contactDetails/contactDetails.js
+++ components/contactDetails/contactDetails.js
@@ -303,45 +303,45 @@ export function renderAfterDelete(userId) {
  */
 function renderDetailsTemplate(user) {
   return /*html*/ `   
-      <div class="contactDetailsBox">
-        <div class="headings" >
-            <span class="heading">Contacts</span>
-            <span class="subHeading" >Better with a team</span>
-            <div id="switchMobileButton" class="switchMobileButton" onclick="switchMobile()">${returnIcon("arrowLeft")}</div>            
-        </div>  
-         <div id="userDetailPanel" class="userDetailPanel">
-          <div class="userQuickInfo">
-              <div class="userInitials" style="background-color: ${user[1].user_color};" >
-              ${user[1].profile.first_name.toUpperCase().charAt(0)}  ${user[1].profile.last_name.toUpperCase().charAt(0)}
-              </div>
-              <div class="userActions">
-                  <div class="userName">
-                  ${user[1].profile.first_name} ${user[1].profile.last_name}
-                  </div>
-                  <div id="userProfileButtons" class="userProfileButtons">
-                      <button class="editButton" 
-                      onclick="showEditChosenUserDialog('${user[1].id}')">${returnIcon("edit")}Edit</button>
-                      <button class="deleteButton" onclick="showConfirmDeleteUserDialog('${user[1].id}')">${returnIcon("delete")}Delete</button>
-                  </div>                
-              </div>
-          </div>
-          <div class="contactInfo">
-              <div class="heading">Contact Information</div>                    
-          <div class="contactEmail">
-              <div class="type">Email</div>
-              <a href="mailto:${user[1].profile.email}">${user[1].profile.email}</a>            
-          </div>
-          <div class="contactPhone">
-          <div class="type">Phone</div>
-              <a href="tel:${user[1].profile.phone}">${user[1].profile.phone}</a>
-          </div>
-          </div>  
-          <button onclick="userProfileButtonsMobile()" id="userProfileButtonsMobile" class="userProfileButtonsMobile">
-            <img id="userProfileButtonsMobileImg" class="userProfileButtonsMobileImg" onclick="userProfileButtonsMobile()" src="./assets/icons/more_vert.png" alt="">
-          </button>        
+    <div class="contactDetailsBox">
+      <div class="headings" >
+          <span class="heading">Contacts</span>
+          <span class="subHeading" >Better with a team</span>
+          <div id="switchMobileButton" class="switchMobileButton" onclick="switchMobile()">${returnIcon("arrowLeft")}</div>            
+      </div>  
+        <div id="userDetailPanel" class="userDetailPanel">
+        <div class="userQuickInfo">
+            <div class="userInitials" style="background-color: ${user[1].user_color};" >
+            ${user[1].profile.first_name.toUpperCase().charAt(0)}  ${user[1].profile.last_name.toUpperCase().charAt(0)}
+            </div>
+            <div class="userActions">
+                <div class="userName">
+                ${user[1].profile.first_name} ${user[1].profile.last_name}
+                </div>
+                <div id="userProfileButtons" class="userProfileButtons">
+                    <button class="editButton" 
+                    onclick="showEditChosenUserDialog('${user[1].id}')">${returnIcon("edit")}Edit</button>
+                    <button class="deleteButton" onclick="showConfirmDeleteUserDialog('${user[1].id}')">${returnIcon("delete")}Delete</button>
+                </div>                
+            </div>
         </div>
-      </div> 
-    `;
+        <div class="contactInfo">
+            <div class="heading">Contact Information</div>                    
+        <div class="contactEmail">
+            <div class="type">Email</div>
+            <a href="mailto:${user[1].profile.email}">${user[1].profile.email}</a>            
+        </div>
+        <div class="contactPhone">
+        <div class="type">Phone</div>
+            <a href="tel:${user[1].profile.phone}">${user[1].profile.phone}</a>
+        </div>
+        </div>  
+        <button onclick="userProfileButtonsMobile()" id="userProfileButtonsMobile" class="userProfileButtonsMobile">
+          <img id="userProfileButtonsMobileImg" class="userProfileButtonsMobileImg" onclick="userProfileButtonsMobile()" src="./assets/icons/more_vert.png" alt="">
+        </button>        
+      </div>
+    </div> 
+  `;
 }
 
 /**
@@ -351,12 +351,12 @@ function renderDetailsTemplate(user) {
  */
 function renderDetailsTemplateFallback() {
   return /*html*/ `
-      <div class="contactDetailsBox" >
-        <div class="headings" >
-            <span class="heading">Contacts</span>
-            <span class="subHeading" >Better with a team</span>
-            <div id="switchMobileButton" class="switchMobileButton"  onclick="switchMobile()">${returnIcon("arrowLeft")}</div>
-        </div>
+    <div class="contactDetailsBox" >
+      <div class="headings" >
+          <span class="heading">Contacts</span>
+          <span class="subHeading" >Better with a team</span>
+          <div id="switchMobileButton" class="switchMobileButton"  onclick="switchMobile()">${returnIcon("arrowLeft")}</div>
       </div>
-    `;
+    </div>
+  `;
 }
